3173859f98e9b60952f93e6b942fcf1c75418000
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-03-02 22:05:41 +0200

    add HT1

--- src/components/Article.js
+++ src/components/Article.js
@@ -29,14 +29,3 @@ class Article extends Component {
 }
 
 export default Article
-
-/*
-export default (props) => {
-    const {article} = props
-    return (
-        <div>
-            <h3>{article.title}</h3>
-            <section>{article.text}</section>
-        </div>
-    )
-}*/
